590eb70bccb11d439e21bff1dde02b731f3241f0
Lucas Kahlert <lucas.kahlert@square-src.de>
2019-04-15 01:00:03 +0200

    i3: Add i3 + polybar + rofi + compton configuration

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for MONITOR in $(xrandr -q | grep -w connected | cut -d" " -f1); do
+    POLYBAR_MONITOR=$MONITOR polybar bottom &
+done
+
+# Terminate already running compton instances
+killall -q compton
+
+# Wait until compton have been shutdown
+while pgrep -u $UID -x compton > /dev/null; do sleep 1; done
+
+# Start compton compositor
+compton --daemon
--- install.sh
+++ install.sh
@@ -36,9 +36,10 @@ fi
 ln -vs $__dir__/.virtualenvs/postactivate .virtualenvs/
 
 [ ! -d ".config" ] && mkdir -p .config/
-[ -d ".config/terminator" ] &&  [ ! -L ".config/terminator" ] && mv .config/terminator .config/terminator.old
-[ -L ".config/terminator" ] && rm -fv .config/terminator
-ln -vs $__dir__/terminator .config/
+
+for file in $__dir__/.config/*; do
+    ln -vsf $file $HOME/.config/
+done
 
 # create .ssh/ config directory
 [ ! -d ".ssh" ] && mkdir -p .ssh/
