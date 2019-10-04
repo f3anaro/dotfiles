b592472ecdf8b4851571eb8bdcaff69af95557d9
Lucas Kahlert <lucas.kahlert@cloudandheat.com>
2019-10-04 09:35:55 +0200

    i3: use hostname for default xrandr layout
     - Start redshift by default
     - Restart i3 on screen layout switch
     - Change monitor configuration

--- .config/i3/xrandr.sh
+++ .config/i3/xrandr.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -euo pipefail
 
 # Search all screen layout scripts and pipe it to rofi for selection
 layout=$(find $HOME/.screenlayout/ -type f -printf "%f\n" | rofi -dmenu -p screenlayout)
 
 if [[ $layout ]]; then
     # Execute selected layout
-    exec $HOME/.screenlayout/$layout
+    $HOME/.screenlayout/$layout
+    
+    # Restart i3
+    i3-msg restart
 fi
